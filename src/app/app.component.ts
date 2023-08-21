ad23294f2f39b735e02dec1b353e344888e32053
franklyhuncakes <103598587+franklyhuncakes@users.noreply.github.com>
2023-08-21 09:53:52 -0500

    perm deleted unneccessary header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'local-intro';
+  constructor(){
+
+  }
+  title = 'Title: local-intro';
+  message = 'Hello from app.component.'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IntroHeaderComponent } from './intro-header/intro-header.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroHeaderComponent
   ],
   imports: [
     BrowserModule,
