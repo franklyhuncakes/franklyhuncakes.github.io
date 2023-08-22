6cba61dc45d8367508a09bf1798614d278366d76
franklyhuncakes <103598587+franklyhuncakes@users.noreply.github.com>
2023-08-22 08:08:41 -0500

    created components/home in public/

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
